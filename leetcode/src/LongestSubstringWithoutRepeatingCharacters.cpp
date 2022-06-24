f5bea0030ea467e9d885f28908882170fea0cbe4
qtf0x <vmarias@mines.edu>
2022-06-24 15:19:35 -0600

    optimal solution to problem 3

--- leetcode/src/LongestSubstringWithoutRepeatingCharacters.cpp
+++ leetcode/src/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -7,8 +7,8 @@
  *        repeating characters.
  */
 
+#include <cinttypes>
 #include <iostream>
-#include <unordered_set>
 
 /**
  * @brief Find the length of the longest substring without repeating characters
@@ -36,6 +36,31 @@ int main() {
 }
 
 int lengthOfLongestSubstring(std::string s) {
+    bool window[95] = {0};
+    // problem requirements state 0 <= s.size() <= 5e4 (only need two bytes)
+    uint_fast16_t curr_len(0), max_len(0);
+
+    // loop until second pointer reaches end
+    for (uint_fast16_t i(0), j(0); j < s.size(); ++j) {
+        // move first pointer forward until no repeats
+        for (; window[static_cast<size_t>(s.at(j)) - 32]; ++i) {
+            window[static_cast<size_t>(s.at(i)) - 32] = 0;
+            --curr_len;
+        }
+
+        window[static_cast<size_t>(s.at(j)) - 32] = 1;
+        ++curr_len;
+
+        if (curr_len > max_len) {
+            max_len = curr_len;
+        }
+    }
+
+    return max_len;
+}
+
+/* works, but really slow
+int lengthOfLongestSubstring(std::string s) {
     std::unordered_set<char> window;
     int max_len = 0;
 
@@ -61,4 +86,5 @@ int lengthOfLongestSubstring(std::string s) {
 
     return max_len;
 }
+*/
 
